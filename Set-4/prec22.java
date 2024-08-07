9b7f01304b92cec97793933ce31f4911b1b32fd4
rudrabhavsar7 <23dcs012@charusat.edu.in>
2024-08-07 15:13:22 +0530

    set4

---
+++ Set-4/prec22.java
@@ -0,0 +1,20 @@
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int i=0,sum;
+        for(i=0;i<=;i++)
+        {
+            
+        }
+        return sum;
+    }
+}
+
+public class prec22 {
+    public static void main(String[] args) {
+
+    }
+}
