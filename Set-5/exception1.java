0f57c77cab0bccaca475ab651f92231e475e4d4e
rudrabhavsar7 <23dcs012@charusat.edu.in>
2024-08-07 15:13:34 +0530

    set5

--- Set-5/exception1.java
+++ Set-5/exception1.java
@@ -1,43 +1,45 @@
 
 public class exception1 {
     public static void main(String[] args) {
-        // int a = 3, b = 0, c;
-        // System.out.println("before");
-        // try {
-        // c = a / b;
-        // } catch (Exception e) {
-        // System.out.println("Exception resolved ");
-        // System.out.println(e.toString());
-        // System.out.println(e.getMessage());
-        // e.printStackTrace();
-        // }
+        int a = 25, b = 0,c;
+        System.out.println("before");
+        try {
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("10/0");
+            System.exit(0);
+        }
+        System.out.println("End");
 
         // System.out.println("After");
 
-        int a[] = { 0, 1, 2 };
-        try {
-            System.out.println(a[3]);
-        } catch (Exception e) {
-            System.out.println("handeld " + e);
-        }
+        // int a[] = { 0, 1, 2 };
+        // try {
+        // System.out.println(a[3]);
+        // } catch (Exception e) {
+        // System.out.println("handeld " + e);
+        // }
 
-        String s = "Charusat";
-        try {
-            System.out.println(s.charAt(9));
-        } catch (Exception e) {
+        // String s = "Charusat";
+        // try {
+        // System.out.println(s.charAt(9));
+        // } catch (Exception e) {
 
-            System.out.println("Handeled " + e);
-        }
+        // System.out.println("Handeled " + e);
+        // }
 
-        String s1 = null;
-        try
-        {
-            System.out.println(s1.length());
-        }
-        catch(Exception e)
-        {
-            System.out.println("handeled " + e);
+        // String s1 = null;
+        // try
+        // {
+        // System.out.println(s1.length());
+        // }
+        // catch(Exception e)
+        // {
+        // System.out.println("handeled " + e);
 
-        }
+        // }
     }
 }
