d7265465274a75cbc677ae62938087ff8b9a6d37
rudrabhavsar7 <23dcs012@charusat.edu.in>
2024-09-24 10:27:40 +0530

    Set-6

---
+++ Set-6/prec27.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class prec27 {
+    public static void main(String[] args) {
+        int length = args.length;
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            
+            String fname = args[i];
+            
+            File f = new File(fname);
+            count=0;
+
+            try {
+                if (f.createNewFile()) {
+                    System.out.println("File Created");
+                } else {
+                    System.out.println("Already");
+                }
+                Scanner scan = new Scanner(f);
+                while (scan.hasNextLine()) {
+                    String temp = scan.nextLine();
+                    System.out.println(temp);
+                    count++;
+                }
+                scan.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Count  = " + count);
+        }
+
+    }
+}
